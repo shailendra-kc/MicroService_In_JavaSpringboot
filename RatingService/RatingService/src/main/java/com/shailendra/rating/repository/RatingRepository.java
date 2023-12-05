7363b5877dad37d839a0bdaecf2f446ff837a999
shailendra <shailendra.kc2@gmail.com>
2023-12-06 00:18:40 +0530

    Merge remote-tracking branch 'origin/xy' into xy

---
+++ RatingService/RatingService/src/main/java/com/shailendra/rating/repository/RatingRepository.java
@@ -0,0 +1,14 @@
+package com.shailendra.rating.repository;
+
+import com.shailendra.rating.entities.Rating;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+import java.util.List;
+
+public interface RatingRepository extends MongoRepository<Rating,String>
+{
+    //custom finder methods
+    List<Rating> findByUserId(String userId);
+    List<Rating> findByHotelId(String hotelId);
+
+}
---
+++ UserService/UserService/src/main/java/com/shailendra/user/service/repositories/UserRepository.java
@@ -0,0 +1,10 @@
+package com.shailendra.user.service.repositories;
+
+import com.shailendra.user.service.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User,String>
+{
+    //if you want to implement any custom method or query
+    //write
+}
